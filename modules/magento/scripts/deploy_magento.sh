89af1f367d06d92007276669cb299f7b88771d48
Frederic Descamps <lefred.descamps@gmail.com>
2021-03-15 22:49:00 +0100

    fix admin typo
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

--- modules/magento/scripts/deploy_magento.sh
+++ modules/magento/scripts/deploy_magento.sh
@@ -3,7 +3,7 @@
 
 cd /var/www/html
 
-bin/magento setup:install --no-ansi --db-host ${mds_ip}  --db-name ${magento_schema} --db-user ${magento_name} --db-password '${magento_password}' --elasticsearch-host ${opendistro_ip} --elasticsearch-port 9200 --admin-firstname='${magento_admin_firstname}' --admin-lastname='${magento_admin_lastname}' --admin-user=admin='${magento_admin_login}' --admin-password='${magento_admin_password}' --admin-email='${magento_admin_email}'
+bin/magento setup:install --no-ansi --db-host ${mds_ip}  --db-name ${magento_schema} --db-user ${magento_name} --db-password '${magento_password}' --elasticsearch-host ${opendistro_ip} --elasticsearch-port 9200 --admin-firstname='${magento_admin_firstname}' --admin-lastname='${magento_admin_lastname}' --admin-user='${magento_admin_login}' --admin-password='${magento_admin_password}' --admin-email='${magento_admin_email}'
 
 cd /var/www
 chown apache. -R html
